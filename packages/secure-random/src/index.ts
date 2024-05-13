0e30319cd2cf1b3fa08ef92c97888c8ea71d5271
soohyeon-lee <soohyeon.lee@navercorp.com>
2024-05-13 14:52:21 +0900

    :recycle: 난수생성로직 수정

--- packages/secure-random/src/index.ts
+++ packages/secure-random/src/index.ts
@@ -1,7 +1,29 @@
-export const secureRandom = () => {
-    return Math.random()
+const createNodeRandom = (min = 0, max = 1) => {
+    const {randomInt} = require('node:crypto') 
+    return randomInt(Math.ceil(min), Math.floor(max))
 }
 
-export const insecureRandom = () => {
-    return Math.random()
+const createBrowserRandom = (min = 0, max = 1) => {
+    const randomBuffer = new Uint32Array(1);
+    window.crypto.getRandomValues(randomBuffer);
+    const randomNumber = randomBuffer[0] / (0xffffffff + 1);
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(randomNumber * (max - min + 1)) + min;
+}
+
+export const secureRandom = ({min = 0, max = 1}) => {
+    if (min > max) {
+        throw new Error('Invalid Parameter')
+    }
+    
+    return typeof window !== 'undefined' ? createBrowserRandom(min, max) : createNodeRandom(min, max)
+}
+
+export const insecureRandom = ({min = 0, max = 1}) => {
+    if (min > max) {
+        throw new Error('Invalid Parameter')
+    }
+
+    return Math.random() * (max - min) + min;
 }
